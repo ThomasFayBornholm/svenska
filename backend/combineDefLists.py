1b4bb4039ce368bbfb28c5cf59ba517229e3c746
ThomasFayBornholm <thomasfay@btinternet.com>
2022-10-22 14:22:50 +0200

    Simplifies list merge helper scripts

--- backend/combineDefLists.py
+++ backend/combineDefLists.py
@@ -1,15 +1,22 @@
 import sys
+import os
+from os.path import exists
 import json
-if len(sys.argv) < 4:
-    print("usage: " + sys.argv[0] + " infile_1 infile_2 outfile")
+if len(sys.argv) < 2:
+    print("usage: " + sys.argv[0] + " infile_1")
     exit()
-f1 = sys.argv[1]
-f2 = sys.argv[2]
-outName = sys.argv[3]
-with open (f1, 'r', encoding='utf-8') as infile1:
+fileName = sys.argv[1]
+tmpFile = fileName + "_tmp"
+
+if exists(tmpFile):
+    os.remove(tmpFile)
+os.rename(fileName, tmpFile)
+os.system("git checkout -- " + fileName)
+
+with open (fileName, 'r', encoding='utf-8') as infile1:
     json1 = json.load(infile1)
 
-with open(f2, 'r', encoding='utf-8') as infile2:
+with open(tmpFile, 'r', encoding='utf-8') as infile2:
     json2 = json.load(infile2)
    
 words = dict()
@@ -20,6 +27,9 @@ for key in json2:
     if not key in words:
         words[key] = json2[key]
 
-print("Combined wordDefList_1 with " + str(len(json1)) + " words with wordDefList_2 with " + str(len(json2)) + " words to get " + str(len(words)) + " unique words.")
-with open(outName, 'w',encoding='utf-8') as outfile:
+cnt1 = str(len(json1))
+cnt2 = str(len(json2))
+
+print("Combined wordDefList_1 with " + cnt1 + " words with wordDefList_2 with " + cnt2 + " words to get " + str(len(words)) + " unique words.")
+with open(fileName, 'w',encoding='utf-8') as outfile:
     json.dump(words, outfile)
--- backend/combineLists.py
+++ backend/combineLists.py
@@ -1,14 +1,19 @@
 import sys
-if len(sys.argv) < 4:
-    print("usage: " + sys.argv[0] + " infile_1 infile_2 outfile")
+import os
+from os.path import exists
+if len(sys.argv) < 2:
+    print("usage: " + sys.argv[0] + " infile_1")
     exit()
-f1 = sys.argv[1]
-f2 = sys.argv[2]
-outName = sys.argv[3]
-with open(f1, 'r', encoding='utf-8') as infile_1:
+fileName = sys.argv[1]
+tmpFile = fileName + "_tmp"
+if exists(tmpFile): 
+    os.remove(tmpFile)
+os.rename(fileName, tmpFile)
+os.system("git checkout -- " + fileName)
+with open(fileName, 'r', encoding='utf-8') as infile_1:
     wordList_1 = infile_1.readlines()
 
-with open(f2, 'r', encoding='utf-8') as infile_2:
+with open(tmpFile, 'r', encoding='utf-8') as infile_2:
     wordList_2 = infile_2.readlines()
 
 words = list()
@@ -21,7 +26,7 @@ for w in wordList_2:
 
 words.sort()
 print("Combined wordList_1 with " + str(len(wordList_1)) + " words with wordList_2 with " + str(len(wordList_2)) + " words to get " + str(len(words)) + " unique words.")
-with open(outName, 'w',encoding='utf-8') as outfile:
+with open(fileName, 'w',encoding='utf-8') as outfile:
     for w in words:
         outfile.write(w)
 
