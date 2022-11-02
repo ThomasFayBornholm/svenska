e6207ef545315fce5f7fc0a6dee6813503a4f1a6
ThomasFayBornholm <thomasfay@btinternet.com>
2022-11-02 23:31:03 +0100

    Improves definition lookups

--- backend/getDefAll.php
+++ backend/getDefAll.php
@@ -4,6 +4,9 @@
 	$trail = "-def";
 	$classArr = array("verb", "adjektiv", "adverb", "substantiv_en", "substantiv_ett");
 	$out = "";
+	// HTML escape chars
+	// Choose to do dynamic HTML formatting here as easier to handle each item directly
+	$tmp = "<span onclick=setClass(&#34;_c_&#34;,&#34;$word&#34;)><i>_c_</i>:</span><br>";
 	foreach ($classArr as $el) {
 		$name = $el . $trail;
 		$contents = file_get_contents($path . $name, 'UTF-8');
@@ -14,7 +17,7 @@
 			$del = "";
 		}
 		if (array_key_exists($word, $dict)) {
-			$out = $out . $del . $el . ":<br>" . $dict[$word];
+			$out = $out . $del . str_replace("_c_",$el,$tmp) . $dict[$word];
 		}
 	}
 	if (strlen($out) === 0) {	
