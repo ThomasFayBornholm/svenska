c3ad1f79297261cd1c254c16e83a3ec8acf96838
ThomasFayBornholm <thomasfay@btinternet.com>
2022-11-06 18:18:43 +0100

    Improves mobile lookups for referenced words.

--- backend/syncToMobile.py
+++ backend/syncToMobile.py
@@ -1,4 +1,11 @@
 import json
+templateList='''<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string-array name="<class>_words">
+<vallist>
+    </string-array>
+</resources>
+'''
 template='''<?xml version="1.0" encoding="utf-8"?>
 <resources>
     <string-array name="<arr_1>">
@@ -13,6 +20,22 @@ wordClasses=("verb", "adjektiv", "adverb", "substantiv_en", "substantiv_ett", "t
 path = 'C:/Users/glatho01/AndroidStudioProjects/Svenska/app/src/main/res/values'
 print("Writing output to path: " + path)
 for c in wordClasses:
+    # Word listing
+    name = c + "-only";
+    out = ""
+    strOut = templateList
+    with open(name, 'r', encoding = 'utf-8') as infile:
+        words = infile.readlines()
+    print(len(words))
+    for w in words:
+        out += "        <item>" + w.replace("\n","") + "</item>\n"
+    strOut = strOut.replace("<class>",c)
+    strOut = strOut.replace("<vallist>",out)
+    name = name + ".xml"
+    print("Writing " + str(len(words)) + " words to " + name)
+    with open(path + "/" + name, 'w', encoding = 'utf-8') as outfile:
+        outfile.write(strOut);
+    # Definition list has different life-cycle from word list
     # Mobile platform needs separate arrays for the keys and values and will combine internally
     name = c + "-def"
     with open(name, 'r', encoding = 'utf-8') as infile:
@@ -32,7 +55,7 @@ for c in wordClasses:
         strOut = strOut.replace("<valList>", strVals)
         strOut = strOut.replace("<br>","_br_")
         strOut = strOut.replace("<nedsättande>","(nedsättande)")
-        strOut = strOut.replace("'",'"')
+        strOut = strOut.replace("'","\\\"")
         print("Writing " + str(len(jsonDict)) + " defs to " + name + ".xml")
         outfile.write(strOut)
 
