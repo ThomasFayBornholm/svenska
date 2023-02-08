a82cd1f26aece64d614296752e66bad03175a045
ThomasFayBornholm <thomas.fay@btinternet.com>
2023-02-08 18:37:12 +0100

    Generalises backup script for easier maintenance.

--- backend/backup.ps1
+++ backend/backup.ps1
@@ -1,13 +1,12 @@
-$arr = @("all", "verb", "adjektiv", "adverb", "substantiv_en", "substantiv_ett", "test");
 
 $dest="backup"
-for ($i = 0; $i -lt $arr.count; $i++) {
-	$name = $arr[$i] + "-only";
-	Copy-Item $name $dest;
-	$name = $arr[$i] + "-def";
-	Copy-Item $name $dest;
-	$name = $arr[$i] + "-meta";
-	Copy-Item $name $dest;
-}
+$name = "*-only";
+Copy-Item $name $dest;
+$name = "*-def";
+Copy-Item $name $dest;
+$name = "*-meta";
+Copy-Item $name $dest;
+$name = "*-more";
+Copy-Item $name $dest;
 
 Copy-Item -recurse -force C:\xampp\htdocs\svenska "C:\Users\SETHFAY\"
