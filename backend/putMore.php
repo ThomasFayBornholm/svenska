50d65872f235efffa4ce26ef52c08c46004c81f2
ThomasFayBornholm <thomasfay@btinternet.com>
2023-02-08 22:20:22 +0100

    Simplifies interface to add additional definition information

--- backend/putMore.php
+++ backend/putMore.php
@@ -1,43 +1,16 @@
 <?php
 	$path = getcwd() ."/";
-	$class = $_GET['class'];
-	$header = $_GET['header'];
-	$word = $_GET['word'];
+	$class = $_GET['class'];	
+	$key = $_GET['key'];
 	$more = $_GET['more'];
 	$trail = "-more";
+	// Existing definitions are overwritten
 	if (strlen($more) > 0) {
 		$name = $class . $trail;
 		$contents = file_get_contents($path . $name, 'UTF-8');
 		$arr = json_decode($contents,true);
-		// Replicate the dictionary headers		
-		$existing = "";
-		if ($arr != null) {
-			if (array_key_exists($word,$arr)) {
-				$existing = $arr[$word] . "<br>";
-			}
-		}
-		$hFind = $header . ":<br>";
-		// Existing information is overwritten
-		if (str_contains($existing,$hFind)) {
-			$start = strpos($existing, $hFind);
-			$nextHeader = strpos($existing,":<br>",$start);
-			/*
-			$len = strpos($existing, ":<br>",$start + strlen($hFind)) - $start;			
-			$replace = substr($existing, $start, $len);
-			
-			$existing = str_replace($replace,$hFind . $more,$existing);			
-			*/
-			$str = "";
-		} else {
-		// New information is added
-			$str = $header . ":<br>" . $more;
-		}
-		if (substr($str,-4) === "<br>") {
-			$str = substr($str,0,(strlen($str)-4));
-		}
-		// Crude hack for now.
-		$existing = str_replace("<br><br>","<br>",$existing);
-		$arr[$word] = $existing . $str;
+		// Replicate the dictionary headers				
+		$arr[$key]=$more;				
 		file_put_contents($path. $name, json_encode($arr));
 	}
 	echo json_encode("");
