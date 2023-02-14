d6cdce54826e317c3bd5b34db1b4d4d6a38bbe5d
ThomasFayBornholm <thomas.fay@btinternet.com>
2023-02-14 14:05:10 +0100

    Bug-fix for words where no score already exists.

--- backend/changeScore.php
+++ backend/changeScore.php
@@ -8,17 +8,18 @@
 	$dict = json_decode($contents, JSON_UNESCAPED_UNICODE);
 	$out = array();
 	$out["res"] = "fail";
+	$old = 0;
 	if (array_key_exists($word, $dict)) {
 		$old = $dict[$word];
-		
-		$new = (int)$old + $diff;
-		if ($new > 2) $new = 0;
-		if ($new < 0) $new = 2;
-		$out["old"]=$old;
-		$out["new"]=$new;
-		$dict[$word]=$new;
-		file_put_contents($path. $fName, json_encode($dict));
-		$out["res"] = "success";		
 	}
+	$new = (int)$old + $diff;
+	if ($new > 2) $new = 0;
+	if ($new < 0) $new = 2;
+	$out["old"]=$old;
+	$out["new"]=$new;
+	$dict[$word]=$new;
+	file_put_contents($path. $fName, json_encode($dict));
+	$out["res"] = "success";		
+	
 	echo json_encode($out);
 ?>
