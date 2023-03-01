63290c0b792cdca58907ba68be611a5e1962b6ec
ThomasFayBornholm <thomas.fay@btinternet.com>
2023-03-01 14:15:21 +0100

    Adds mathc count to regexSeek for ease of debugging.

--- backend/regexSeek.php
+++ backend/regexSeek.php
@@ -7,12 +7,13 @@
 	$contents = file_get_contents($path . $name, 'UTF-8');
 	$words = preg_split("/\r\n|\r|\n/", $contents);
 
-	$out = array();
+	$out["matches"] = array();
+	$out["count"] = 0;
 	foreach($words as $w) {
 		if (preg_match($regex,$w)) {
-			array_push($out,$w);
+			array_push($out["matches"],$w);
 		}
 	}
-	
+	$out["count"] = count($out["matches"]);
 	echo json_encode($out);
 ?>
