a9d2ec84b1b92c49a419b80a5d5a8cd7fb6b5f69
ThomasFayBornholm <thomas.fay@btinternet.com>
2023-05-22 18:58:19 +0200

    Improves conjugation matching.

--- backend/getConj.php
+++ backend/getConj.php
@@ -8,20 +8,84 @@
 		}
 		return $tmp;
 	}
+	
+	function wordMatch($word, $comp) {
+		$match1 = $word === $comp;
+		$fuzzyE = str_replace("é","e",$comp);
+		$fuzzyE = str_replace("é","e",$fuzzyE);
+		$match2 = $word === $fuzzyE;				
+		$res = $match1 || $match2;
+		return $res;
+	}
+	
+	function containsWord($word, $conjArr) {
+		$res = false;		
+		foreach($conjArr as $el) {			
+			if (wordMatch($word, $el)) {
+				$res = true;
+				break;
+			}
+		}		
+		return $res;
+	}
+	
+	function containsAllWords($wordList, $conjArr) {
+		$nWords = count($wordList);
+		if (count($conjArr) === 0) {
+			return false;
+		}
+		
+		$nMatches = 0;
+		
+		foreach($wordList as $w) {			
+			foreach ($conjArr as $el) {
+				if (wordMatch($el,$w)) {
+					$nMatches++;
+					break;
+				}
+			}
+		}
+		$res = $nMatches === $nWords;
+		return $res;
+	}
+	
+	function getConjugations($key, $dict) {		
+		if (array_key_exists($key, $dict)) {
+			$lines = explode("<br>", $dict[$key]);					
+			$lineOne = $lines[0];					
+			// First explosion just to get root word
+			$conjugations = explode(" ", $lineOne);				
+			$root = $conjugations[0];			
+			$lineOne = str_replace(", presens", "", $lineOne);
+			$lineOne = str_replace("eller ", "", $lineOne);
+			$lineOne = str_replace("~", $root, $lineOne);
+			$conjugations = explode(" ", $lineOne);
+		} else {
+			// Empty, no conjugations could be retrieved;
+			$conjugations = array();
+		}
+		if (count($conjugations) > 10) {
+			$conjugations = array();
+		}
+		return $conjugations;
+	}
+	
 	$path = getcwd() ."/";
 	$word = $_GET['word'];
 	$rest = "";		
 	$nFind = 1;
 	if (str_contains($word, " ")) {
 		$tmp = explode(" ", $word);
-		$word = $tmp[0];
+		$wordRoot = $tmp[0];
 		$rest = "";
 		$nFind = count($tmp);
+		$del = "";
 		for ($i = 1; $i < $nFind; $i++) {
-			$rest = $rest . " " . $tmp[$i];
+			$rest .= $del . $tmp[$i];
+			$del = " ";
 		}
 	}
-
+	
 	$class = $_GET['class'];
 	$trail = "-meta";
 	if ($class === "all") {
@@ -46,40 +110,44 @@
 		$wordList = preg_split("/\r\n|\r|\n/", $contents);
 		$contentsMeta = file_get_contents($path . $name, 'UTF-8');
 		$dict = json_decode($contentsMeta, JSON_UNESCAPED_UNICODE);
-		
-		foreach($wordList as $key) {	
-			if (array_key_exists($key, $dict)) {
-				$lines = explode("<br>", $dict[$key]);
+		if ($el === "verb" && $nFind > 1) {
+			$restList = explode(" ", $rest);			
+			foreach($wordList as $key) {				
+				$conjugations = getConjugations($key, $dict);					
+				if (containsWord($wordRoot, $conjugations)) {												
+					if (containsAllWords($restList, $conjugations)) {
+						$out["class"] = $el;
+						$out["word"] = $key;
+						echo json_encode($out);
+						return;
+					}
+				}
+			}
+		// Adverbs need to give the following behaviour:
+		// "i morse": 'i morse eller imorse'
+		// "imorse": 'i morse eller imorse'
+		} else if ($el === "adverb" && strlen($rest) > 0) {
+			$restList = explode(" ", $rest);			
+			var_dump($restList);
+			return;
+			foreach($wordList as $key) {				
+				$conjugations = getConjugations($key, $dict);					
 				
-				$lineOne = $lines[0];
-				$lineOne = repText($lineOne,$replacements,$word);
-							
-				
-				$words = explode(" ",$lineOne);
-				
-				$firstConj = $words[0];
-				foreach($words as $w) {
-
-					$w = str_replace("~",$firstConj, $w);					
-					$fuzzyE = str_replace("é","e",$w);
-					$fuzzyE = str_replace("é","e",$fuzzyE);
-					
-					if ($word === $w or $word === $fuzzyE or (str_contains($lineOne,$word) and $el === "adverb")) {		
-						if (str_contains($lineOne,$rest)) {				
-							$keyCount = count(explode(" ", $key));	
-							// Enforce same number of words in "stored key" (from file) and "search word" (from user)
-							// Exception of above for adverbs, e.g. "i kväll" and "ikväll" shall be equivalent.
-							if ($nFind === $keyCount || $el === "adverb") {
-								if ($el === "adverb") {
-																								
-								}
-								$out["class"] = $el;
-								$out["word"] = $key;
-								echo json_encode($out);
-								return;
-							}
-						}
-					}					
+				if (containsAllWords($restList, $conjugations)) {					
+					$out["class"] = $el;
+					$out["word"] = $key;
+					echo json_encode($out);
+					return;
+				}
+			}
+		} else {			
+			foreach($wordList as $key) {
+				$conjugations = getConjugations($key, $dict);					
+				if (containsWord($word, $conjugations)) {					
+					$out["class"] = $el;
+					$out["word"] = $key;
+					echo json_encode($out);
+					return;
 				}
 			}
 		}
