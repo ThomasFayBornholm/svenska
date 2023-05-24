c29620997fc9bbb06b42c82ae0f70bbe6f9bda15
ThomasFayBornholm <thomas.fay@btinternet.com>
2023-05-24 18:58:45 +0200

    Updates formatting of construction text in 'more' information.

--- backend/showMore.php
+++ backend/showMore.php
@@ -7,10 +7,35 @@
 	$contents = file_get_contents($path . $name, 'UTF-8');
 	$dict = json_decode($contents, JSON_UNESCAPED_UNICODE);	
 	$out = "";
+	$tmp = "";
 	if ($dict) {
 		if (array_key_exists($word, $dict)) {
-			$out = $dict[$word];
+			$tmp = $dict[$word];
 		}
 	}
+	
+	$tmpArr = explode("<br>",$tmp);
+	
+	$isCon = false;
+	$del = "";	
+	foreach($tmpArr as $l) {		
+		if (str_contains($l,"KONSTRUKTION:")) {						
+			$isCon = true;
+		} else if (str_contains($l,"EXEMPEL:")) {
+			$isCon = false;
+		} else if (str_contains($l,"HISTORIK:")) {
+			$isCon = false;
+		}
+		
+		if ($isCon) {
+			$out .= $del . "○ " . $l;
+		} else {
+			$out .= $del . $l;
+		}
+		$del = "<br>";
+	}	
+	// Hack 
+	$out = str_replace("○ KONSTRUKTION:" ,"KONSTRUKTION: ",$out);
+	$out = str_replace("○","&nbsp;&nbsp;○", $out);
 	echo json_encode($out);
 ?>
