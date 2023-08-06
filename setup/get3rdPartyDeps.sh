4708c9eec76be8451f0a53c04320e7ecf9f4c9db
glatho01 <glatho01@debian>
2023-08-06 21:23:51 +0200

    Adds Unix helper scripts.

---
+++ setup/get3rdPartyDeps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+curl https://code.jquery.com/jquery-3.6.0.js -o "jquery-3.6.0.js"
+curl "https://ajax.googleapis.com./ajax/libs/jquery/3.6.0.js" -o "../jquery-3.6.0.js"
+curl "https://code.jquery.com/ui/1.13.2/jquery-ui.js" -o "../jquery-ui.js"
+curl "http://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css" -o "../jquery-ui.css"
