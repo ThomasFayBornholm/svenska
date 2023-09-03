dc0c10596a6e326c35c5070422fb7d61bb18e1ec
ThomasFayBornholm <thomasfay@btinternet.com>
2023-09-03 13:42:40 +0200

    Removes superseded helper scripts.

--- sounds/removeEmpty.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem tmp -Filter *.mp3 | Foreach-Object {
-	$name = $_.Name
-	$file = Get-Item "./tmp/$name";
-	if ($file.length -eq 0) {
-		Write-Output "Removing ./tmp/$name";
-		Remove-Item "./tmp/$name";
-	}
-}
