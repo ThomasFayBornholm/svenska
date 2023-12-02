58ab8a211a653b3350244fc4b02063e7e2d00016
Thomas Fay <thomas.fay@corpowerocean.com>
2023-12-02 11:50:14 +0100

    Updates random word functionality.

--- backend/randomWord.php
+++ backend/randomWord.php
@@ -4,20 +4,20 @@
 	$name = $class . "-score";
 	$contents = file_get_contents($path . $name, 'UTF-8');
 	$dict = json_decode($contents, JSON_UNESCAPED_UNICODE);
-	$out = array();
+	$tmp = array();
 	foreach($dict as $key=>$val) {		
 		$score = (int)$val;
 		if ($score < 2) {
-			array_push($out, $key);
+			array_push($tmp, $key);
 		}
 	}
 
-	$randNum = rand(0,count($out));
-	$tmpWord = $out[$randNum];
+	$randNum = rand(0,count($tmp));
+	$tmpWord = $tmp[$randNum];
 
 	$arr = array();
-	$nameAll = $path . "all-only";
-	$contents = file_get_contents($nameAll, 'UTF-8');
+	$fileName = $path . $class . "-only";
+	$contents = file_get_contents($fileName, 'UTF-8');
 	$words = preg_split("/\r\n|\r|\n/", $contents);
 	
 	$i = 0;
