267ca7e868997bbbe4e13ad37ef2ff8fced146cf
Thomas Fay <thomasfay@btinternet.com>
2025-03-22 10:48:45 +0100

    Include audio setup in install script.

--- setup/install.sh
+++ setup/install.sh
@@ -16,4 +16,12 @@ cp -r $root_path/backend $install_path
 cp -r $root_path/vocab $install_path
 cp -r $root_path/svenska.css $install_path
 cp -r $root_path/img $install_path
+mkdir -p $install_path/sounds
+touch $install_path/sounds/mapping
+mkdir -p $install_path/sounds/en
+mkdir -p $install_path/sounds/ett
+mkdir -p $install_path/sounds/verb
+mkdir -p $install_path/sounds/adj
+mkdir -p $install_path/sounds/adv
+sudo chown -R www-data:www-data $install_path/sounds/*
 wget -O "$install_path/jquery-3.6.0.js" https://code.jquery.com/jquery-3.6.0.js
