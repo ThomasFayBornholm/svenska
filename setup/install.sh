acfecf776d47ce611efb1f14c5e86f6604dbec56
Thomas Fay <thomasfay@btinternet.com>
2025-03-25 13:29:46 +0100

    Complete installation script.

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 set -e
+sudo rm -rf /var/www/html/svenska
 sudo apt install apache2 php php-curl
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 post=${script_dir#*svenska}
@@ -12,7 +13,7 @@ install_path=/var/www/html/svenska
 sudo mkdir -p $install_path
 sudo chown -R $user:$user $install_path
 cp -r $root_path/lists $install_path
-sudo chown www-data:www-data $install_path/lists
+sudo chown -R www-data:www-data $install_path/lists
 cp -r $root_path/backend $install_path
 cp -r $root_path/vocab $install_path
 cp -r $root_path/svenska.css $install_path
@@ -26,5 +27,9 @@ mkdir -p $install_path/sounds/adj
 mkdir -p $install_path/sounds/adv
 sudo chown -R www-data:www-data $install_path/sounds/*
 wget -O "$install_path/jquery-3.6.0.js" https://code.jquery.com/jquery-3.6.0.js
+
+wget -O "$install_path/jquery-ui-1.14.1.zip" https://jqueryui.com/resources/download/jquery-ui-1.14.1.zip
+eval "unzip $install_path/jquery-ui-1.14.1.zip -d $install_path"
+
 
 sudo systemctl restart apache2
